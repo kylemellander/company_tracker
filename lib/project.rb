e23b385334f3c40821f6214b12ee9e96bf613f44
Ryan Heupler & Kyle Mellander <student@epicodus.com>
2015-08-24 11:01:22 -0700

    shows employees in division array

--- lib/project.rb
+++ lib/project.rb
@@ -1,3 +1,3 @@
 class Project < ActiveRecord::Base
-  # has_many(:employees)
+  has_many(:employees)
 end
--- spec/divisions_spec.rb
+++ spec/divisions_spec.rb
@@ -1,8 +1,16 @@
 require ('spec_helper')
 
 describe(Division) do
+  before do
+    @division = Division.create({:name => "Party Division"})
+    @employee = Employee.create({f_name: "Ryan", l_name: "Heupler", division_id: @division.id})
+  end
+  
   it('Verifies Active Record set up properly') do
-    division = Division.create({:name => "Party Division"})
-    expect(Division.all()).to(eq([division]))
+    expect(Division.all()).to(eq([@division]))
+  end
+
+  it("pulls an array of all employees in a division") do
+    expect(@division.employees).to eq ([@employee])
   end
 end
